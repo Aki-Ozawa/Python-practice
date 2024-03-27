a6fb4ada4f7f31a112f34b487782149cde14dd01
小澤章記 <aki@MacBook-Pro.local>
2024-03-27 21:04:49 +0900

    done

--- definition.py
+++ definition.py
@@ -7,7 +7,7 @@ def greet_user ():
 print("Start")
 greet_user()
 print("Finish")
-
+ 
 
 def greet_user(name):
     print(f'Hi {name}!')
@@ -16,3 +16,19 @@ def greet_user(name):
 print("Start")
 greet_user("John")
 print("Finish")
+
+def greet_user(first_name, last_name):
+    print(f'Hi {first_name} {last_name} !')
+    print('Welcome abroad')
+
+greet_user("Aki", "Ozawa")
+greet_user(last_name="Ozawa", first_name="Kinako")
+
+def square(number):
+    return number * number
+
+result = square(3)
+print(result)  
+
+# By default all functions in Python return none. So if you have a function that 
+# calculates something, you can return the result using the return statement
